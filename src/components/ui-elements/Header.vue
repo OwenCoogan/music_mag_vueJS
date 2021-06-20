5073b046db4d913ea89ba979c167e89f30aa900e
Redak45 <43347242+Redak45@users.noreply.github.com>
2021-06-20 22:43:10 +0200

    Update Header.vue

--- src/components/ui-elements/Header.vue
+++ src/components/ui-elements/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header>
-        <nav class="flex items-center justify-between flex-wrap bg-teal p-6">
+        <nav class="flex items-center justify-between bg-green-300 w-full p-6">
           <h1> Music Mag </h1>
           <router-link to="/">Home</router-link>
           <router-link to="/artists">Artists</router-link>
