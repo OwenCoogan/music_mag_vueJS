54c178329693a07b10203563e149057d3fc141d8
Redak45 <43347242+Redak45@users.noreply.github.com>
2021-06-20 22:48:19 +0200

    Update Header.vue

--- src/components/ui-elements/Header.vue
+++ src/components/ui-elements/Header.vue
@@ -1,10 +1,10 @@
 <template>
     <header>
-        <nav class="flex items-center justify-between bg-green-300 w-full p-6">
-          <h1> Music Mag </h1>
-          <router-link to="/">Home</router-link>
-          <router-link to="/artists">Artists</router-link>
-          <router-link to="/articles">News</router-link>
+        <nav class="flex items-center justify-between flex-wrap bg-green-300 w-full p-6"> 
+          <h1 class="text-indigo-50"> Music Mag </h1>
+          <router-link to="/" class="text-indigo-50">Home</router-link>
+          <router-link to="/artists" class="text-indigo-50">Artists</router-link>
+          <router-link to="/articles" class="text-indigo-50">News</router-link>
         </nav>
     </header>
 </template>
